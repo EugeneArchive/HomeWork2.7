44156ae6f6574f53ef4794fcf35d87e0127afb10
Eugene <89818121788@mail.ru>
2022-08-01 12:46:32 +0300

    Second commit (после разбора)

--- src/main/java/com/example/HomeWork27/Employee.java
+++ src/main/java/com/example/HomeWork27/Employee.java
@@ -42,6 +42,14 @@ public class Employee {
     public int hashCode() {
         return Objects.hash(surname, name);
     }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
 
 
